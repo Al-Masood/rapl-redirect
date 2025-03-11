4091b19c89287d5ce516ee15d92447eba8c34c1d
Abdullah Al Masood <aalmasood22@gmail.com>
2025-03-11 10:14:39 +0600

    all links redirected

--- src/redirect.js
+++ src/redirect.js
@@ -1,16 +1,23 @@
-// src/RedirectPage.js
 import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const RedirectPage = () => {
   useEffect(() => {
     window.location.replace('https://rapl.site');
   }, []);
 
-  return (
-    <div>
-      <p>Redirecting to rapl.site...</p>
-    </div>
-  );
+  return <p>Redirecting to rapl.site...</p>;
 };
 
-export default RedirectPage;
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Other routes if needed */}
+        <Route path="*" element={<RedirectPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
